435947fea7e5edaf3cced2ed5f75809af4da4ef0
Lud <luasanchezl@udistrital.edu.co>
2026-03-26 12:01:10 -0500

    devops: implementación lineamientos

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,31 +7,31 @@ import (
 
 func init() {
 
-    beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"],
-        beego.ControllerComments{
-            Method: "Post",
-            Router: "/",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"],
-        beego.ControllerComments{
-            Method: "GetAll",
-            Router: "/",
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           "/",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"],
-        beego.ControllerComments{
-            Method: "GetOne",
-            Router: "/:id",
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/consecutivos_crud/controllers:ConsecutivoController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           "/:id",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 }
